afb302a2596bb6bedb93daefe57ad35e31b63bfd
Jacob Way <jd.way23@gmail.com>
2022-06-18 16:55:21 -0500

    Created Energy Bounds and energy simulations

--- Energy Bounds/model.py
+++ Energy Bounds/model.py
@@ -7,8 +7,11 @@ def testMatrix(m,n):
 def energy(M):
     svd = np.linalg.svd(M)[1]
     return svd.sum()
-def norm(M,n):
-    return np.power(np.absolute(M),n).sum()**(1/n)
+def norm(M):
+    m = 0
+    for i in range(len(M[0])):
+        m = max(m,sum(abs(M[:,i])))
+    return m
 def trace(M):
     m = len(M)
     s = 0
@@ -18,22 +21,45 @@ def trace(M):
 def bounds(M):
     m=len(M)
     n=len(M[0])
-    upper = norm(M,1)/(m*n)**0.5 + ((m-1)*(trace(np.matmul(M,np.transpose(np.conjugate(M))))-norm(M,1)**2/m/n))**0.5
-    return abs(upper)
+    upper = norm(M)/(m*n)**0.5 + ((m-1)*(trace(np.matmul(M,np.transpose(np.conjugate(M))))-norm(M)**2/m/n))**0.5
+    sigs = np.linalg.svd(M)[1]
+    sig1 = sigs[0]
+    sig2 = sigs[1]
+    lower = sig1 + (trace(np.matmul(M,np.transpose(np.conjugate(M))))-sig1**2)/sig2
+    return abs(lower),abs(upper)
 
-
-
-def testEnergies():
-    tests = np.zeros(1000)
-    for i in range(1000):
+def testEnergies(n):
+    tests = np.zeros(n)
+    for i in range(n):
         tests[i] = energy(testMatrix(10,10))
     plt.hist(tests,bins=100)
     plt.show()
-testBounds = np.zeros(1000)
-testEnergies = np.zeros(1000)
-for i in range(1000):
-    M = testMatrix(10,10)
-    testBounds[i]=bounds(M)
-    testEnergies[i] = energy(M)
-plt.hist(testBounds-testEnergies,bins=100)
-plt.show()
+def testBounds(n):
+    testBounds = np.zeros(n)
+    testEnergies = np.zeros(n)
+    for i in range(n):
+        M = testMatrix(10,10)
+        testBounds[i]=bounds(M)[1]
+        testEnergies[i] = energy(M)
+    plt.hist(testBounds-testEnergies,bins=100)
+    plt.show()
+def allBounds(n):
+    lowers = np.zeros(n)
+    actuals = np.zeros(n)
+    uppers = np.zeros(n)
+    for i in range(n):
+        M = np.random.normal(0,1,[20,20])
+        l,u = bounds(M)
+        lowers[i] = l
+        actuals[i] = energy(M)
+        uppers[i] = u
+    plt.subplot(3,1,1)
+    plt.hist(lowers,bins=100)
+    plt.hist(actuals,bins=100)
+    plt.hist(uppers,bins=100)
+    plt.subplot(3,1,2)
+    plt.hist(uppers-actuals,bins=100)
+    plt.subplot(3,1,3)
+    plt.hist(actuals-lowers,bins=100)
+    plt.show()
+allBounds(10000)
