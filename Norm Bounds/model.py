0d567dbb8aa2fd9da86c3682b5a4d42e1d744fee
Jacob Way <jd.way23@gmail.com>
2022-06-18 17:09:03 -0500

    Coded Norm Bounds

--- Norm Bounds/model.py
+++ Norm Bounds/model.py
@@ -1,3 +1,15 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import sys
+
+def spectralNorm(M):
+    return np.linalg.svd(M)[1][0]
+def suBound(M):
+    #Lower bound 2.4
+    return ((np.matmul(np.transpose(np.conjugate(M)),M)).sum()/len(M))**0.5
+diffs = np.zeros(10000)
+for i in range(10000):
+    M = np.random.normal(0,1,[10,10])
+    diffs[i]=spectralNorm(M)-suBound(M)
+plt.hist(diffs,bins = 100)
+plt.show()
